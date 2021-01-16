771d1be78961751a92f84ac9b62b9c255231c53a
stphnsmpsn <mail@stephensampson.dev>
2021-01-16 13:45:52 -0400

    Create HelloWorld React App

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello, World!
+        </p>
+        <a
+          className="GitHub-Link"
+          href="https://github.com/stphnsmpsn"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My GitHub
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
